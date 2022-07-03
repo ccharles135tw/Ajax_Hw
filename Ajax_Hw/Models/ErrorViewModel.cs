6f5b653864d590703993cb66605223ec47d8be86
ccharles135tw@gmail.com <ccharles135tw@gmail.com>
2022-07-03 13:36:12 +0800

    加入專案檔案。

---
+++ Ajax_Hw/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ajax_Hw.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
